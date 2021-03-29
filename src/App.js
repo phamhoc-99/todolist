4206584970cee98807ce61aaeebfcc3021bc4b97
phamhoc-99 <hoanghoatham1999@gmail.com>
2021-03-29 16:37:51 +0700

    done todolist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React ,{Component} from 'react';
 import './App.css';
 import TodoItem from './componentB/TodoItem'; 
+import tick from './img/tick.svg';
 class App extends Component{      
 
   constructor(){
     super();
     this.state={
-    todoItems:[ 
+      newItems:'',   //quản lí state khi nhập xong input sẽ clear
+   
+      todoItems:[ 
     {title:'di xem phim',isComplete:true},
     {title:'di choi'},
     {title:'di ve nha'} 
   ]
-}}
+}
+this.onKeyUp=this.onKeyUp.bind(this);//xử lí lỗi this undefine:trong constructer
+this.onChange=this.onChange.bind(this);
+}
 
   onItemClicked(item){//3.Handling events-setState
 return (event)=>
@@ -32,11 +38,47 @@ this.setState({
  };
 }
 
+onKeyUp(event){
+if(event.keyCode===13){
+let text=event.target.value;
+if(!text){return;}
+
+text=text.trim();
+if(!text){return;}
+
+this.setState({
+ newItem:'', //đặt lại input khi nhập xong
+  todoItems:[
+    {title:text,isComplete:false},
+    ...this.state.todoItems
+  ]
+});}
+
+
+}
+//xử lí sự kiện khi click:keyCode nút enter là 13,
+//event.target:cái đang được trỏ ,
+//event.target.value:giá trị đang viết
+onChange(event){
+  this.setState({
+    newItem: event.target.value
+ });
+}
+
 render(){
-  const{todoItems}=this.state;
+  const{todoItems,newItem}=this.state;//quản lí state
   if(todoItems.length>0){        //2.Conditional rendering
     return (
 <div className="App"> 
+<div className="Header">
+<img src={tick} width={32} height={32} />
+<input 
+  type="text" 
+  placeholder="Add a new item" 
+  value={newItem}//value và onChange phải đi cùng nhau
+  onChange={this.onChange}
+  onKeyUp={this.onKeyUp} />
+</div>
   {todoItems.map((item,index)=> 
    <TodoItem 
      key={index} 
--- src/componentB/TodoItem.js
+++ src/componentB/TodoItem.js
@@ -1,14 +1,23 @@
 import React, { Component }  from 'react';
 import './TodoItem.css';   //import css
+import checkImg from'../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 class TodoItem extends Component {
  
 render(){
   let className ='TodoItem';//1.thêm class dựa vào thuộc tính:dynamic class name
   const {item,onClick}=this.props;
+
+  let url=checkImg;
+if(item.isComplete){
+  url=checkCompleteImg;
+}
+
 if(item.isComplete){className+=' TodoItem-complete';}  //nhớ thêm dấu cách vào ,ko là nó gộp thành 1 class
 
         return (  
-            <div onClick={onClick} className={className}>
+            <div  className={className}>
+                <img onClick={onClick} src={url} width={32} height={32} />  
                 <p>{this.props.item.title}</p>       
             </div> 
             ); 
